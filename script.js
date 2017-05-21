12e1059d16e20224d845d6f55c8d432496f03681
wer1K <dimaminchenko@gmail.com>
2017-05-21 23:33:51 +0300

    Change <input> to <select>

--- script.js
+++ script.js
@@ -12,5 +12,12 @@ function f(){
   document.getElementById('sum_per_number_of_days').value = Number(price_per_day) * Number(amount_of_days);
   let sum_per_number_of_days = document.getElementById('sum_per_number_of_days').value;
   let discount_per_day = Number(price_per_day) * Number(discount);
-  document.getElementById('sum_per_days_with_discount').value = Number(sum_per_number_of_days) - Number(discount_per_day);
+  let sum_per_days_with_discount = document.getElementById('sum_per_days_with_discount').value = Number(sum_per_number_of_days) - Number(discount_per_day);
+  let balance = document.getElementById('balance').value;
+
+  if (balance > sum_per_days_with_discount) {
+    document.getElementById('amount_of_days').value = Number(amount_of_days) + 1;
+    console.log(amount_of_days + " " + sum_per_days_with_discount);
+    document.getElementById('result').innerHTML = amount_of_days + " " + sum_per_days_with_discount;
+  }
 }
